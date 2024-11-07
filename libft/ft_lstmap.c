f289e70303871dcc689cc831b36a6bd2a4cf5456
Grace Mary O'Donnell <gro-donn@c1r4s4.42lisboa.com>
2024-11-07 17:21:24 +0000

    final fix

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -3,49 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gro-donn <gro-donn@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gro-donn <gro-donn@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:18:51 by gro-donn          #+#    #+#             */
-/*   Updated: 2024/11/06 13:18:52 by gro-donn         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:30:59 by gro-donn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    if(!lst )
+    t_list *new_list;
+    t_list *tmp; 
+    tmp = new_list; 
+    
+    if(!lst || !f || !del)
         return  NULL;
-
-    t_list *ptr = lst;
+    
+    new_list = ft_lstnew(lst->content); 
+    
     while(ptr !=NULL)
     {
-        if(ptr -> content != NULL)
-        {
-            ft_lstdelone(ptr, del);
-        }
-        ptr->next;
-        f(ptr -> content);
-  
+        
+
     }
     return ptr;
 }
-
-void ft_print(void *content)
-{
-    printf("%s", (char *)content);
-}
-
-int main()
-{
-    t_list *lst = ft_lstnew(ft_strdup("Grace"));
-    t_list *second_node = ft_lstnew(ft_strdup("is the"));
-    t_list *final_node = ft_lstnew(ft_strdup("best"));
-
-   ft_lstadd_back( &lst, second_node);
-   ft_lstadd_back(&lst, final_node);
-
-    ft_lstmap(lst, ft_print, ft_lstdelone);
-    return(0);
-}
-*/
