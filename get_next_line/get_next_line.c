85a7765f07a0e0fdb454e9e856931cc5d7ae1907
Grace <20680751+godonnell2@users.noreply.github.com>
2024-11-14 19:59:38 +0000

    minor edits

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,4 +1,5 @@
 
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,11 @@
+#include "get_next_line.h"
+
+char *_fill_line_buffer(int fd, char *left_c, char *buffer)
+{
+
+}
+
+char *_set_line(char *line_buffer)
+{
+
+}
