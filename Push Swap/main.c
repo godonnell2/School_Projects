77ffb6db05c0d700e433defafc603efbab7e3c53
Grace <20680751+godonnell2@users.noreply.github.com>
2025-01-10 21:26:59 +0000

    all working all norm

--- Push Swap/main.c
+++ Push Swap/main.c
@@ -6,7 +6,7 @@
 /*   By: gro-donn <gro-donn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 00:50:59 by gro-donn          #+#    #+#             */
-/*   Updated: 2025/01/10 21:01:54 by gro-donn         ###   ########.fr       */
+/*   Updated: 2025/01/10 21:25:14 by gro-donn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,33 +20,40 @@ void	cleanup(t_stack *a, t_stack *b)
 	free_stack(&b);
 }
 
-int	main(int ac, char **av)
+int	handle_args(int ac, char ***av)
 {
-	t_stack	*stack_a;
-	t_stack	*stack_b;
-	char	**split_args;
 	int		split_used;
+	char	**split_args;
 
-	stack_a = NULL;
-	stack_b = NULL;
-	split_args = NULL;
 	split_used = 0;
-	if (ac < 2)
-		return (0);
 	if (ac == 2)
 	{
-		split_args = ft_split(av[1], ' ');
+		split_args = ft_split((*av)[1], ' ');
 		if (!split_args || !is_valid_split(split_args))
 		{
 			free_arr(ac, split_args);
 			err_case_nofree();
 		}
 		ac = count_split_args(split_args);
-		av = split_args;
+		*av = split_args;
 		split_used = 1;
 	}
-	check_args(ac, av);
-	printf("ac:%d\n", ac);
+	check_args(ac, *av);
+	return (split_used);
+}
+
+int	main(int ac, char **av)
+{
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	int		split_used;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	split_used = 0;
+	if (ac < 2)
+		return (0);
+	split_used = handle_args(ac, &av);
 	stack_a = add_args_to_stack(ac, av);
 	if (!check_sorting(&stack_a))
 	{
