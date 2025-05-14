09267a8cf38e8d0645e297b766a8e5434a2a4f84
Grace <20680751+godonnell2@users.noreply.github.com>
2025-05-14 21:02:34 +0100

    builtins echo exit code 2

---
+++ minishell/bis2.c
@@ -0,0 +1,325 @@
+#include "minishell.h"
+
+// do we have to update the path name before the semicolon??
+/*
+Symbolic Links (Symlinks): Unlike hard links, symlinks are independent files
+that store a pointer to the target. They can cross filesystems and can
+reference directories.
+A symlink is a file that contains a path (either relative or absolute)
+to another file or directory. When you access the symlink,
+the operating system automatically redirects you to the target file or
+directory.
+t’s similar to a shortcut in Windows, but at the filesystem level.
+
+*/
+
+
+/*
+enviro vars are key-value pairs that a process inherits from its parent. 
+These are used to pass configuration information.
+extern means it's held in an external file or lib  <unistd.h> 
+*/
+void	initialize_env_list(t_env_vars **env_list)
+{
+	extern char	**environ;
+	int			i;
+	char		*env_var;
+	char		*equal_sign;
+
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		env_var = environ[i];
+		equal_sign = strchr(env_var, '=');
+		if (equal_sign)
+		{
+			*equal_sign = '\0';
+			set_env_var(env_list, env_var, equal_sign + 1);
+			*equal_sign = '=';
+		}
+		i++;
+	}
+}
+// if (input[i] == '\0' || input[i] == '~' || input[i] == '-')
+// chdir interprets it relative to the curr dir so works for both rel/abs
+
+// do we need all space chars no that's ºarser job??
+
+static int	get_current_directory(char *buf, size_t size)
+{
+	if (!getcwd(buf, size))
+	{
+		perror("cd: get cwd");
+		return (0);
+	}
+	return (1);
+}
+
+int	ft_cd(char **input, t_env_vars **env_list)
+{
+	char	old_pwd[10000];
+	char	new_pwd[10000];
+
+	if (!input[1])
+	{
+		perror("cd: missing argument");
+		return (1);
+	}
+	if (!get_current_directory(old_pwd, sizeof(old_pwd)))
+		return (1);
+	if (chdir(input[1]) != 0)
+	{
+		perror("failure to chdir in cd");
+		return (1);
+	}
+	if (!get_current_directory(new_pwd, sizeof(new_pwd)))
+		return (1);
+	set_env_var(env_list, "OLDPWD", old_pwd);
+	set_env_var(env_list, "PWD", new_pwd);
+	return (0);
+}
+
+// ECHO // Print failure (rare) 2
+int	ft_echo(char **args)
+{
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	if (args[i] && ft_strcmp(args[i], "-n") == 0)
+	{
+		newline = 0;
+		i++;
+	}
+	while (args[i])
+	{
+		if (printf("%s", args[i]) < 0)
+			return (2); 
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+	return (0);
+}
+
+// ENV
+//empty list is not an error 
+// only read only so just did single ptr
+int	env(t_env_vars *head)
+{
+	if (!head)
+	{
+		return (0);
+	}
+	while (head)
+	{
+		if (head->key && head->value)
+			printf("%s=%s\n", head->key, head->value);
+		head = head->next;
+	}
+	return (0);
+}
+
+
+void clean_env_lst(t_env_vars **env_vars)
+{
+    t_env_vars *current = *env_vars;
+    while (current != NULL) 
+	{
+        t_env_vars *next = current->next;
+        free(current->key);
+        free(current->value);
+        free(current);
+        current = next;
+    }
+}
+// EXIT we are feeding a double ptr so hence no addrss
+int	exit_shell(t_env_vars **env_list)
+{
+	printf("exit\n");
+	clean_env_lst(env_list);
+	return (0);
+}
+
+
+// so if we have a signal we want to return 1 or some other number
+
+// EXPORT
+/*
+The export command in Unix/Linux shells is used to set environment variables,
+making them available to child processes of the current shell session.
+
+Current Shell Only: By default, variables are local to the current shell.
+export makes them available to subprocesses (like scripts or commands you run).
+
+*/
+/*
+handle case sensitivity this is correct right as bash treats them separately
+do i need to check validity of export key words??
+plus need to handle malloc fail in strdup right now im just losing the var!!!!
+
+nheritance: When a process creates a child process (using fork),
+	the child process inherits a copy of the parent's environment. However,
+	by default,
+	not all variables from the parent process are passed to the child.
+ Only the variables that have been explicitly "exported" are included.
+ // NEED TO REMEMBER TO FREE
+*/
+void	set_env_var(t_env_vars **env_list, const char *key, const char *value)
+{
+	t_env_vars	*current;
+	t_env_vars	*new_node;
+
+	current = *env_list;
+	while (current)
+	{
+		if (ft_strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = ft_strdup(value);
+			return ;
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(t_env_vars));
+	if (!new_node)
+	{
+		perror("malloc failed in set_env_var");
+		return ;
+	}
+	new_node->key = ft_strdup(key);
+	new_node->value = ft_strdup(value);
+	new_node->next = *env_list;
+	*env_list = new_node;
+}
+
+// when no equal sign is found we set the key with empty value
+// is this what bash does
+// kk no bash returns empty so need to add empty string still stores value
+// NOT USING MY STRCHR I DONT KNOW WHY 
+static int	handle_export_arg(t_env_vars **env_list, char *arg)
+{
+	char	*equal_sign;
+	char	*temp;
+	int		result;
+
+	temp = ft_strdup(arg);
+	if (!temp)
+	{
+		perror("export: ft_strdup");
+		return (1);
+	}
+	equal_sign = strchr(temp, '=');
+	result = 0;
+	if (equal_sign)
+	{
+		*equal_sign = '\0';
+		set_env_var(env_list, temp, equal_sign + 1);
+	}
+	else
+	{
+		set_env_var(env_list, temp, "");
+	}
+	free(temp);
+	return (result);
+}
+
+int	ft_export(t_env_vars **env_list, char **args)
+{
+	int	i;
+	int	status;
+
+	if (!args[1])
+		return (0);
+	status = 0;
+	i = 1;
+	while (args[i] && status == 0)
+	{
+		status = handle_export_arg(env_list, args[i]);
+		i++;
+	}
+	return (status);
+}
+
+// print working directory
+int	ft_pwd(void)
+{
+	char	pwd[100000];
+	int		exit_code;
+
+	if (!getcwd(pwd, sizeof(pwd)))
+	{
+		perror("pwd:get cwd failed");
+		return (exit_code = 1);
+	}
+	else
+	{
+		printf("%s\n", pwd);
+		return (exit_code = 0);
+	}
+}
+
+static t_env_vars	*extract_node(t_env_vars **head, char *key)
+{
+	t_env_vars	*curr;
+	t_env_vars	*prev;
+
+	curr = *head;
+	prev = NULL;
+	while (curr)
+	{
+		if (ft_strcmp(curr->key, key) == 0)
+		{
+			if (prev)
+				prev->next = curr->next;
+			else
+				*head = curr->next;
+			return (curr);
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+	return (NULL);
+}
+
+int	ft_unset(t_env_vars **head, char *key)
+{
+	t_env_vars	*node_to_remove;
+
+	if (!head || !key)
+	{
+		return (1);
+	}
+	printf("Attempting to unset key: %s\n", key);
+	node_to_remove = extract_node(head, key);
+	if (node_to_remove)
+	{
+		free(node_to_remove->key);
+		free(node_to_remove->value);
+		free(node_to_remove);
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
+// Note: Typically unset returns success even if var didn't exist
+// hence the else
+
+/*
+Deletes shell/environment variables so they no longer exist.
+Affects Current Shell Only
+Changes are not inherited by child processes (unlike export).
+Works on Functions Too
+Can also remove shell functions:
+No Effect on Read-Only Variables
+Fails silently (or with an error) if the variable is marked readonly.
+
+Command	Description
+unset VAR	Removes the variable VAR.
+
+*/
---
+++ minishell/executor2.c
@@ -0,0 +1,831 @@
+#include "minishell.h"
+#include <fcntl.h>
+#include <stdio.h> //has to come before readline
+#include <stdlib.h>
+#include <sys/wait.h> // NEED ADD TO HEADER FILE ASK BEATH
+#include <unistd.h>
+#define READ 0
+#define WRITE 1
+#define STDIN 0
+#define SPLIT_BUFF_SIZE 102400
+
+// AsK BETH IF WE CAN ADD TO HEAD THIS EXEC STRUCT
+typedef struct s_executor
+{
+	int			pipe_fd[2];
+	int			prev_pipe_fd;
+	pid_t		*pids;
+	int			last_exit_code;
+	int cmd_count;       // Total commands in pipeline
+	int cmd_i;           // Current command index (renamed)
+	char **env_array;    // Environment variables
+	t_command *commands; // Commands array
+}				t_executor;
+
+typedef struct s_split_state
+{
+	size_t		buff_offset;
+	size_t		word_count;
+	char		**arr;
+	size_t		word;
+	size_t		word_len;
+}				t_split_state;
+
+// Function prototypes
+
+void			check_command_in_path(char **path_arr, char *cmd,
+					char *full_path);
+char			*get_env_path_variable(char **envp);
+void			resolve_command_full_path(t_env_vars *env_vars, char *cmd,
+					char *full_path);
+
+int	ft_wordlen(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i] && s[i] != ' ')
+		i++;
+	return (i);
+}
+
+void	ft_putstring_fd(char *s, int fd)
+{
+	write(fd, s, ft_wordlen(s));
+}
+
+char	*strndup(const char *s, size_t n)
+{
+	char	*d;
+
+	d = malloc(n + 1);
+	if (!d)
+		return (NULL);
+	ft_memcpy(d, s, n);
+	d[n] = '\0';
+	return (d);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*cs1;
+	unsigned char	*cs2;
+
+	i = 0;
+	cs1 = (unsigned char *)s1;
+	cs2 = (unsigned char *)s2;
+	while (i < n && (cs1[i] || cs2[i]))
+	{
+		if (cs1[i] != cs2[i])
+		{
+			return (cs1[i] - cs2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_strcpy(char *src, char *dst)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
+
+char	*cat_strs_char(char *dest, const char *src1, char c, const char *src2)
+{
+	size_t	i;
+
+	i = 0;
+	if (!src1 || !src2)
+	{
+		return (NULL);
+	}
+	while (*src1)
+	{
+		dest[i++] = *src1++;
+	}
+	dest[i++] = c;
+	while (*src2)
+	{
+		dest[i++] = *src2++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	word_count;
+
+	word_count = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+			word_count++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (word_count);
+}
+
+static void	skip_separators(const char **s, char sep)
+{
+	while (**s == sep)
+	{
+		(*s)++;
+	}
+}
+
+char	**ft_split_buff(char const *s, char sep, void *buff)
+{
+	const char		*start;
+	t_split_state	curr;
+
+	curr.buff_offset = 0;
+	curr.word_count = count_words(s, sep);
+	curr.arr = (char **)(buff + curr.buff_offset);
+	curr.buff_offset += sizeof(char *) * (curr.word_count + 1);
+	curr.word = 0;
+	while (*s)
+	{
+		skip_separators(&s, sep);
+		start = s;
+		while (*s && *s != sep)
+			s++;
+		curr.word_len = s - start;
+		if (curr.buff_offset + curr.word_len + 1 > SPLIT_BUFF_SIZE)
+			return (NULL);
+		curr.arr[curr.word] = (char *)buff + curr.buff_offset;
+		ft_memcpy(curr.arr[curr.word], start, curr.word_len);
+		curr.arr[curr.word++][curr.word_len] = '\0';
+		curr.buff_offset += curr.word_len + 1;
+		skip_separators(&s, sep);
+	}
+	curr.arr[curr.word] = NULL;
+	return (curr.arr);
+}
+
+static char	*create_env_string(char *key, char *value)
+{
+	int		len_key;
+	char	*env_str;
+	int		len_total;
+
+	len_key = ft_wordlen(key);
+	len_total = len_key + ft_wordlen(value) + 2;
+	env_str = malloc(len_total);
+	if (!env_str)
+		return (NULL);
+	ft_strcpy(env_str, key);
+	env_str[len_key] = '=';
+	ft_strcpy(env_str + len_key + 1, value);
+	return (env_str);
+}
+
+static int	count_env_nodes(t_env_vars *env_vars)
+{
+	int	count;
+
+	count = 0;
+	while (env_vars)
+	{
+		count++;
+		env_vars = env_vars->next;
+	}
+	return (count);
+}
+
+// IS FREEING CORECT ???
+// printf("ENV: %s=%s\n", curr->key, curr->value);
+char	**convert_env_to_array(t_env_vars *env_vars)
+{
+	int			count;
+	t_env_vars	*curr;
+	char		**env_arr;
+	int			i;
+
+	count = count_env_nodes(env_vars);
+	env_arr = malloc(sizeof(char *) * (count + 1));
+	if (!env_arr)
+		return (NULL);
+	curr = env_vars;
+	i = 0;
+	while (curr)
+	{
+		env_arr[i] = create_env_string(curr->key, curr->value);
+		if (!env_arr[i])
+		{
+			free_env_vars(env_vars);
+			return (NULL);
+		}
+		curr = curr->next;
+		i++;
+	}
+	env_arr[i] = NULL;
+	return (env_arr);
+}
+
+void	free_env_array(char **env_array)
+{
+	char	**tmp;
+
+	if (!env_array)
+		return ;
+	tmp = env_array;
+	while (*tmp)
+	{
+		free(*tmp);
+		tmp++;
+	}
+	free(env_array);
+}
+
+// TOO LONG
+// static int	execute_builtin(t_command *command, t_env_vars **env_vars)
+// {
+// 	const char	*cmd = command->args[0];
+// 	int			i;
+
+// 	if (!command || !command->args || !command->args[0])
+// 	{
+// 		printf("Error: Invalid command structure\n");
+// 		return (1);
+// 	}
+// 	if (ft_strcmp(cmd, "cd") == 0)
+// 		return (ft_cd(command->args, env_vars));
+// 	else if (ft_strcmp(cmd, "echo") == 0)
+// 		return (ft_echo(command->args));
+// 	else if (ft_strcmp(cmd, "pwd") == 0)
+// 		return (ft_pwd());
+// 	else if (ft_strcmp(cmd, "export") == 0)
+// 		return (ft_export(env_vars, command->args));
+// 	else if (ft_strcmp(cmd, "unset") == 0)
+// 	{
+// 		i = 1;
+// 		while (command->args[i])
+// 		{
+// 			if (ft_unset(env_vars, command->args[i]) != 0)
+// 				return (1);
+// 			i++;
+// 		}
+// 		return (0);
+// 	}
+// 	else if (ft_strcmp(cmd, "env") == 0)
+// 		return (env(*env_vars));
+// 	else if (ft_strcmp(cmd, "exit") == 0)
+// 		return (exit_shell(env_vars));
+// 	return (2);
+// }
+
+char	*get_path_variable(t_env_vars *env_vars)
+{
+	t_env_vars	*current;
+
+	current = env_vars;
+	while (current != NULL)
+	{
+		if (ft_strncmp(current->key, "PATH", 5) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
+
+void	check_command_in_path(char **path_arr, char *cmd, char *full_path)
+{
+	size_t	p_len;
+	size_t	c_len;
+	char	**temp_patharr;
+
+	temp_patharr = path_arr;
+	while (*temp_patharr != NULL)
+	{
+		p_len = ft_wordlen(*temp_patharr);
+		c_len = ft_wordlen(cmd);
+		if ((p_len + c_len + 2) > PATH_MAX)
+			return ;
+		cat_strs_char(full_path, *temp_patharr, '/', cmd);
+		if (access(full_path, X_OK) == 0)
+			return ;
+		temp_patharr++;
+	}
+	full_path[0] = '\0';
+}
+
+void	resolve_command_full_path(t_env_vars *env_vars, char *cmd,
+		char *full_path)
+{
+	char	buff[SPLIT_BUFF_SIZE];
+	char	**path_arr;
+	char	*path_env;
+
+	full_path[0] = '\0';
+	if (access(cmd, X_OK) == 0)
+	{
+		ft_strcpy(cmd, full_path);
+		return ;
+	}
+	path_env = get_path_variable(env_vars);
+	if (!path_env)
+		return ;
+	path_arr = ft_split_buff(path_env, ':', buff);
+	check_command_in_path(path_arr, cmd, full_path);
+}
+
+static void	setup_input_redirection(t_command *cmd)
+{
+	int	fd;
+
+	if (!cmd->infile)
+		return ;
+	fd = open(cmd->infile, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(cmd->infile);
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+}
+
+// bitwise ops
+// write only for curr process
+// TRUNC does not append it overwrites,
+// needs to clear it otherwise when writing could have left over bits
+// 0 means its in octal form (base 8)
+// 6 owner Read (4) + Write (2) = 6
+// 4 group Read (4) = 4
+// 4 others Read (4) = 4
+static void	setup_output_redirection(t_command *cmd)
+{
+	int	fd;
+	int	flags;
+
+	if (!cmd->outfile)
+		return ;
+	flags = O_WRONLY | O_CREAT;
+	if (cmd->append_out)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	fd = open(cmd->outfile, flags, 0644);
+	if (fd < 0)
+	{
+		perror(cmd->outfile);
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+}
+
+static void	setup_redirections(t_command *cmd)
+{
+	setup_input_redirection(cmd);
+	setup_output_redirection(cmd);
+}
+
+void	init_executor(t_executor *ex, t_command *cmds, int count, char **env)
+{
+	ex->cmd_i = 0;
+	ex->cmd_count = count;
+	ex->commands = cmds;
+	ex->env_array = env;
+	ex->prev_pipe_fd = -1;
+	ex->pids = malloc(sizeof(pid_t) * count);
+}
+
+void	close_unused_fds(t_executor *ex)
+{
+	if (ex->cmd_i > 0)
+		close(ex->prev_pipe_fd);
+	if (ex->cmd_i < ex->cmd_count - 1)
+		close(ex->pipe_fd[WRITE]);
+}
+
+int	needs_pipe(t_executor *ex)
+{
+	return (ex->cmd_i < ex->cmd_count - 1);
+}
+
+void	create_pipe(t_executor *ex)
+{
+	if (pipe(ex->pipe_fd) < 0)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int	fork_and_run_child(t_executor *ex)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	ex->pids[ex->cmd_i] = pid;
+	return (pid);
+}
+
+void	execute_child(t_executor *ex)
+{
+	t_command	*cmd;
+
+	cmd = &ex->commands[ex->cmd_i];
+	if (cmd->heredoc_fd != -1)
+		dup2(cmd->heredoc_fd, STDIN_FILENO);
+	else if (ex->cmd_i > 0)
+		dup2(ex->prev_pipe_fd, STDIN_FILENO);
+	if (ex->cmd_i < ex->cmd_count - 1)
+		dup2(ex->pipe_fd[WRITE], STDOUT_FILENO);
+	close_unused_fds(ex);
+	setup_redirections(cmd);
+	if (cmd->full_path && access(cmd->full_path, X_OK) == 0)
+		execve(cmd->full_path, cmd->args, ex->env_array);
+	perror("execve");
+	exit(EXIT_FAILURE);
+}
+
+void	handle_parent(t_executor *ex)
+{
+	if (ex->cmd_i > 0)
+		close(ex->prev_pipe_fd);
+	if (needs_pipe(ex))
+	{
+		close(ex->pipe_fd[WRITE]);
+		ex->prev_pipe_fd = ex->pipe_fd[READ];
+	}
+	if (ex->commands[ex->cmd_i].heredoc_fd != -1)
+		close(ex->commands[ex->cmd_i].heredoc_fd);
+}
+
+void	wait_all(t_executor *ex)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	while (i < ex->cmd_count)
+	{
+		waitpid(ex->pids[i], &status, 0);
+		if (WIFEXITED(status))
+			ex->last_exit_code = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			ex->last_exit_code = 128 + WTERMSIG(status);
+		i++;
+	}
+}
+
+void	cleanup_executor(t_executor *ex)
+{
+	free(ex->pids);
+}
+
+
+void	execute_pipes(t_command *cmds, int count, char **env)
+{
+	t_executor	ex;
+
+	init_executor(&ex, cmds, count, env);
+	while (ex.cmd_i < ex.cmd_count)
+	{
+		if (needs_pipe(&ex))
+			create_pipe(&ex);
+		if (fork_and_run_child(&ex) == 0)
+			execute_child(&ex);
+		handle_parent(&ex);
+		ex.cmd_i++;
+	}
+	wait_all(&ex);
+	cleanup_executor(&ex);
+}
+
+static int	is_built_in(t_command *cmd)
+{
+	const char	*builtins[] = {"echo", "cd", "pwd", "export", "unset", "env",
+			"exit", NULL};
+	const char	**builtin_ptr = builtins;
+
+	if (!cmd || !cmd->args[0])
+		return (0);
+	while (*builtin_ptr)
+	{
+		if (ft_strcmp(cmd->args[0], *builtin_ptr) == 0)
+		{
+			return (1);
+			break ;
+		}
+		builtin_ptr++;
+	}
+	return (0);
+}
+
+void	resolve_all_command_paths(t_env_vars *env_vars, t_command *cmds,
+		int num_cmds)
+{
+	char	full_path[PATH_MAX];
+	int		i;
+	int		builtin;
+
+	i = 0;
+	while (i < num_cmds)
+	{
+		builtin = is_built_in(&cmds[i]);
+		if (!builtin)
+		{
+			resolve_command_full_path(env_vars, cmds[i].args[0], full_path);
+			if (full_path[0] != '\0')
+			{
+				cmds[i].full_path = ft_strdup(full_path);
+			}
+		}
+		else
+		{
+			cmds[i].full_path = NULL;
+		}
+		i++;
+	}
+}
+// void setup_commands(t_command *commands)
+// {
+//     commands[0].args = malloc(sizeof(char *) * 3);
+//     commands[0].args[0] = strdup("ls");
+//     commands[0].args[1] = strdup("-l");
+//     commands[0].args[2] = NULL;
+//     commands[0].infile = NULL;
+//     commands[0].outfile = NULL;
+//     commands[0].append_out = 0;
+// }
+// void setup_commands(t_command *commands)
+// {
+//     commands[0].args = malloc(sizeof(char *) * 4);
+//     commands[0].args[0] = strdup("echo");
+//     commands[0].args[1] = strdup("-n");
+//     commands[0].args[2] = "this is a test";
+//     commands[0].args[3] = NULL;
+//     commands[0].infile = NULL;
+//     commands[0].outfile = NULL;
+//     commands[0].append_out = 0;
+//     commands[0].full_path = NULL;
+// }
+// void	setup_commands(t_command *commands)
+// {
+// 	commands[0].args = malloc(sizeof(char *) * 3);
+// 	commands[0].args[0] = strdup("cd");
+// 	commands[0].args[1] = NULL;
+// 	commands[0].args[2] = NULL;
+// 	commands[0].infile = NULL;
+// 	commands[0].outfile = NULL;
+// 	commands[0].append_out = 0;
+// 	commands[0].full_path = NULL;
+// }
+
+// void	setup_commands(t_command *commands)
+// {
+// 	commands[0].args = malloc(sizeof(char *) * 3);
+// 	commands[0].args[0] = strdup("export");
+// 	commands[0].args[1] =  strdup("MYVAR=test_value");
+// 	commands[0].args[2] = NULL;
+// 	commands[0].infile = NULL;
+// 	commands[0].outfile = NULL;
+// 	commands[0].append_out = 0;
+// 	commands[0].full_path = NULL;
+// }
+
+//  PIPES & HEREDOC
+void	setup_commands(t_command *commands)
+{
+	// cat << EOF | grep hello
+	commands[0].args = malloc(sizeof(char *) * 2);
+	commands[0].args[0] = strdup("cat");
+	commands[0].args[1] = NULL;
+	commands[0].infile = NULL;
+	commands[0].outfile = NULL;
+	commands[0].append_out = 0;
+	commands[0].heredoc_eof = strdup("EOF");
+	commands[0].heredoc_fd = ft_heredoc(commands[0].heredoc_eof);
+	commands[1].args = malloc(sizeof(char *) * 3);
+	commands[1].args[0] = strdup("grep");
+	commands[1].args[1] = strdup("hello");
+	commands[1].args[2] = NULL;
+	commands[1].infile = NULL;
+	commands[1].outfile = NULL;
+	commands[1].append_out = 0;
+}
+// void	setup_commands(t_command *commands)
+// {
+// 	// ls -l | grep "d" | wc -l
+// 	commands[0].args = malloc(sizeof(char *) * 3);
+// 	commands[0].args[0] = strdup("ls");
+// 	commands[0].args[1] = strdup("-l");
+// 	commands[0].args[2] = NULL;
+// 	commands[0].infile = NULL;
+// 	commands[0].outfile = NULL;
+// 	commands[0].append_out = 0;
+// 	commands[1].args = malloc(sizeof(char *) * 3);
+// 	commands[1].args[0] = strdup("grep");
+// 	commands[1].args[1] = strdup("d");
+// 	commands[1].args[2] = NULL;
+// 	commands[1].infile = NULL;
+// 	commands[1].outfile = NULL;
+// 	commands[1].append_out = 0;
+// 	commands[2].args = malloc(sizeof(char *) * 3);
+// 	commands[2].args[0] = strdup("wc");
+// 	commands[2].args[1] = strdup("-l");
+// 	commands[2].args[2] = NULL;
+// 	commands[2].infile = NULL;
+// 	commands[2].outfile = NULL;
+// 	commands[2].append_out = 0;
+// }
+// void	setup_commands(t_command *commands)
+// {
+// 	commands[0].args = malloc(sizeof(char *) * 4);
+// 	commands[0].args[0] = strdup("unset");
+// 	commands[0].args[1] = strdup("VAR1");
+// 	commands[0].args[2] = strdup("VAR2");
+// 	commands[0].args[3] = NULL;
+// 	commands[0].infile = NULL;
+// 	commands[0].outfile = NULL;
+// 	commands[0].append_out = 0;
+// }
+
+// multiple pipes
+
+int	main(void)
+{
+	t_env_vars	*env_vars;
+	t_command	*commands;
+	char		**env_array;
+	t_env_vars	*path_var;
+	t_env_vars	*home_var;
+	char		full_path[PATH_MAX];
+	int			num_commands;
+
+	env_vars = NULL;
+	num_commands = 2;
+	// 1. Create minimal environment
+	path_var = malloc(sizeof(t_env_vars));
+	path_var->key = strdup("PATH");
+	path_var->value = strdup("/bin:/usr/bin:/usr/local/bin");
+	path_var->next = NULL;
+	home_var = malloc(sizeof(t_env_vars));
+	home_var->key = strdup("HOME");
+	home_var->value = strdup("/Users/grace");
+	home_var->next = path_var;
+	env_vars = home_var;
+	// 2. Setup pipeline commands
+	commands = malloc(sizeof(t_command) * num_commands);
+	setup_commands(commands);
+	// 3. Resolve command paths
+	for (int i = 0; i < num_commands; i++)
+	{
+		if (!commands[i].full_path)
+		{ // Only resolve if not builtin
+			resolve_command_full_path(env_vars, commands[i].args[0], full_path);
+			if (full_path[0] != '\0')
+			{
+				commands[i].full_path = strdup(full_path);
+			}
+		}
+	}
+	// 4. Convert environment to array
+	env_array = convert_env_to_array(env_vars);
+	// 5. Execute the pipeline
+	execute_pipes(commands, num_commands, env_array);
+	// 6. Cleanup
+	clean_env_lst(&env_vars);
+	free(env_array);
+	return (0);
+}
+
+// check export main
+// int	main(void)
+// {
+// 	char		**env_array;
+// 	int			exit_code;
+// 	t_command	*commands;
+// 	int			num_commands;
+// 	char		*args1[] = {"export", "VAR1=value1", NULL};
+// 	char		*args2[] = {"export", "VAR2=value2", NULL};
+
+// 	t_env_vars *env_vars = NULL; // Start with empty environment
+// 	num_commands = 1;
+// 	// 1. Initialize environment from actual environment variables
+// 	initialize_env_list(&env_vars);
+// 	// 2. Setup command(s)
+// 	// 3. Resolve command path if needed
+// 	ft_export(&env_vars, args1);
+// 	ft_export(&env_vars, args2);
+// 	printf("Before unset:\n");
+// 	print_env_vars(env_vars);
+// 	commands = malloc(sizeof(t_command) * num_commands);
+// 	setup_commands(commands); // Your existing command setup
+// 	resolve_all_command_paths(env_vars, commands, num_commands);
+// 	// 5. Execute command
+// 	printf("Running unset builtin for key: %s\n", commands[0].args[1]);
+// 	exit_code = execute_builtin(&commands[0], &env_vars);
+// 	if (exit_code == 2) // Not a builtin
+// 	{
+// 		env_array = convert_env_to_array(env_vars);
+// 		execute_pipes(commands, num_commands, env_array);
+// 		free_env_array(env_array);
+// 	}
+// 	printf("after unset:\n");
+// 	print_env_vars(env_vars);
+// 	// CHECK EXPORT DONT KNOW WHY THIS ISNT WORKING CAN SEE IN ENV
+// 	// t_env_vars *node = get_env_node(env_vars, "MYVAR");
+// 	//     if (node && strcmp(node->value, "test_value") == 0) {
+// 	//     printf("✅ Export worked!\n");
+// 	// }
+// 	// else {
+// 	//     printf("❌ Export failed\n");
+// 	// }
+// 	// 7. Cleanup
+// 	free_env_vars(env_vars);
+// 	return (exit_code);
+// }
+
+// TEST FOR CD NEED TO PRINT BEFORE AND AFTER PATH TO PROVE
+// int	main(void)
+// {
+// 	t_env_vars	*home_var;
+// 	t_env_vars	*env_vars;
+// 	int			num_commands;
+// 	t_command	*commands;
+// 	char		full_path[PATH_MAX];
+// 	char		current_dir[PATH_MAX];
+// 	int			exit_code;
+// 	char		**env_array;
+
+// 	// 1. Create a mock envir lst
+// 	home_var = malloc(sizeof(t_env_vars));
+// 	home_var->key = strdup("HOME");
+// 	home_var->value = strdup("/Users/grace");
+// 	home_var->next = NULL;
+// 	env_vars = malloc(sizeof(t_env_vars));
+// 	env_vars->key = strdup("PATH");
+// 	env_vars->value = strdup("/bin:/usr/bin:/usr/local/bin");
+// 	env_vars->next = home_var;
+// 	num_commands = 1;
+// 	commands = malloc(sizeof(t_command) * num_commands);
+// 	setup_commands(commands);
+// 	if (ft_strcmp(commands[0].args[0], "pwd") != 0)
+// 	{ // Only resolve for non-builtins
+// 		resolve_command_full_path(env_vars, commands[0].args[0], full_path);
+// 		if (full_path[0] != '\0')
+// 		{
+// 			commands[0].full_path = strdup(full_path);
+// 		}
+// 	}
+// 	// 4. Exec builitins
+// 	if (getcwd(current_dir, sizeof(current_dir)) != NULL)
+// 	{
+// 		printf("Current Directory (Before cd): %s\n", current_dir);
+// 	}
+// 	else
+// 	{
+// 		perror("getcwd");
+// 	}
+// 	exit_code = execute_builtin(&commands[0], env_vars);
+// 	printf("--- After execute_builtin ---\n");
+// 	printf("Exit code from execute_builtin: %d\n", exit_code);
+// 	if (getcwd(current_dir, sizeof(current_dir)) != NULL)
+// 	{
+// 		printf("Current Directory (After cd): %s\n", current_dir);
+// 	}
+// 	else
+// 	{
+// 		perror("getcwd");
+// 	}
+// 	// exec pipes
+// 	if (exit_code == 2)
+// 	{ // Not a builtin, use execve
+// 		env_array = convert_env_to_array(env_vars);
+// 		exit_code = execute_builtin(commands, env_vars);
+// 		// NEED TO CAPTURE EXIT CODE FROM BELOW TOO!!
+// 		execute_pipes(commands, num_commands, env_array);
+// 		free(env_array);
+// 	}
+// 	printf("exit_code: %i", exit_code);
+// 	// free everythihg in reality
+// 	return (exit_code);
+// }
