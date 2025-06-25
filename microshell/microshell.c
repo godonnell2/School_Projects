a422874c34052c56ba075b7d5bce29b945d99365
Grace <20680751+godonnell2@users.noreply.github.com>
2025-06-25 14:14:16 +0100

    microshell

---
+++ microshell/microshell.c
@@ -0,0 +1,104 @@
+#include "microshell.h"
+
+void ft_putstr_fd2(char *str, char *arg)
+{
+    while(*str)
+        write(2, str++, 1);
+
+    if(arg)
+    {
+        write(2, arg++, 1);
+    }
+    write(2, "\n", 1);
+}
+
+void ft_fatal_error(int fd)
+{
+    if(fd == -1)
+    {
+        ft_putstr_fd2("error_ fatal", NULL);
+        exit(1);
+    }
+}
+
+void exec_child(char **av, int tmp_fd, int i, char **env)
+{
+    av[i] = NULL;
+    ft_fatal_error(dup2(tmp_fd, STDIN_FILENO));
+    close(tmp_fd); 
+
+    execve(av[0], av, env);
+    ft_putstr_fd2("failed to execute", av[0]);
+    exit(1);
+}
+
+int main(int ac, char ** av, char ** env)
+{
+    int fd[2];
+    int i = 0;
+    (void) ac;
+    pid_t pid;
+    int tmp_fd = dup(STDIN_FILENO);
+    ft_fatal_error(tmp_fd);
+
+    while(av[i] && av[i+1])
+    {
+        av = &av[i +1];
+        i = 0;
+        {
+            while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+            {
+                if(strcmp(av[0], "cd")== 0)
+                {
+                    if(i !=2)
+                        ft_putstr_fd2("cd: incorrect number of arguments", NULL);
+                    else if(chdir(av[1]) == 0)
+                        ft_putstr_fd2("cd: failed system error", av[1]);
+                }
+                else if(i != 0 && ((av[i] == NULL) || (strcmp(av[i],";" )== 0)))
+                {
+                    pid = fork();
+                    ft_fatal_error(pid);
+
+                    if(pid == 0)
+                    {
+                        exec_child(av, i, tmp_fd, env);
+                    }
+                    else
+                    {
+                        close(tmp_fd);
+                        while(waitpid(-1, NULL, WUNTRACED)!= -1)
+                        {}
+                        tmp_fd = dup(STDIN_FILENO);
+                        ft_fatal_error(tmp_fd);
+                    }
+
+                }
+                else if(i !=0 && strcmp(av[i], "|"))
+                {
+                    ft_fatal_error(pipe(fd));
+                    pid = fork();
+                    ft_fatal_error(pid);
+
+                    if(pid== 0)
+                    {
+                        ft_fatal_error(dup2(fd[1], STDOUT_FILENO));
+                        close(fd[0]);
+                        close(fd[1]);
+                        exec_child(av, i, tmp_fd, env);
+
+                    }
+                    else
+                    {
+                        close(tmp_fd);
+                        close(fd[1]);
+                        tmp_fd = fd[0];
+                    }
+                }
+                
+            }
+        }
+    }
+    close(tmp_fd);
+    return 0;
+}
---
+++ microshell/microshell.h
@@ -0,0 +1,8 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define STDIN_FILENO 0 
+#define STDOUT_FILENO 1
