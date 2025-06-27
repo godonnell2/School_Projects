82c5fa13647d399a43b081bdbd576755962c55a8
Grace Mary O'Donnell <gro-donn@c1r3s8.42lisboa.com>
2025-06-27 14:25:18 +0100

    min max

--- C02/ex02/Fixed.cpp
+++ C02/ex02/Fixed.cpp
@@ -160,3 +160,20 @@ Fixed Fixed::operator--(int) {
     --_rawBits;
     return temp;
 }
+
+Fixed &Fixed::min(Fixed &a, Fixed &b) {
+    return (a < b) ? a : b;
+}
+
+const Fixed &Fixed::min(const Fixed &a, const Fixed &b) {
+    return (a < b) ? a : b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b) {
+    return (a > b) ? a : b;
+}
+
+const Fixed &Fixed::max(const Fixed &a, const Fixed &b) {
+    return (a > b) ? a : b;
+}
+
