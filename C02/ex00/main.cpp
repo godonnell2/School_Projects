0cd2c6b73699dd72bbb5a9d44d3a1839a9266841
Grace <20680751+godonnell2@users.noreply.github.com>
2025-06-27 09:41:08 +0100

    added 0 rawbits

--- C02/ex00/main.cpp
+++ C02/ex00/main.cpp
@@ -6,5 +6,8 @@ int main() {
     Fixed c;
     c = b;
 
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
     return 0;
 }
