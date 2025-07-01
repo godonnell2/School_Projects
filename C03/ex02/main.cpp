cb9085067fd9953a70106b9121d89e0ed56a0087
Grace <20680751+godonnell2@users.noreply.github.com>
2025-07-01 13:43:16 +0100

    final

--- C03/ex02/main.cpp
+++ C03/ex02/main.cpp
@@ -23,3 +23,21 @@ int main() {
 
     return 0;
 }
+/*
+Even though ScavTrap and FragTrap both inherit from ClapTrap, 
+ClapTrap is only constructed once — this happens because it is marked as a virtual base class
+ or because of constructor resolution using ClapTrap(name + "_clap_name").
+*/
+/*
+both inherit from claptrap and then builds on it
+These show polymorphic behavior:
+ScavTrap Serena aggressively attacks intruder, dealing 20 points of damage!
+FragTrap FR4G launches a huge attack on mutant, dealing 30 damage!
+Polymorphism lets you call the same function on different types of objects and get different behavior.
+high five shows fragtrap has its own unique method
+Derived class destructors run first (FragTrap, ScavTrap)
+Then base class destructor (ClapTrap)
+
+F
+
+*/
