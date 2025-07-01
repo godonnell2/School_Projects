d0979798bc6b4e6f379b2643fbf0588ed3a0472b
Grace <20680751+godonnell2@users.noreply.github.com>
2025-07-01 09:21:10 +0100

    added explanatory notes

--- C03/ex00/main.cpp
+++ C03/ex00/main.cpp
@@ -15,3 +15,19 @@ int main() {
 
     return 0;
 }
+/*
+bot2 (Scavvy) is destroyed first (LIFO order),
+*/
+/*
+
+Inheritance	How a subclass can reuse and extend the functionality of a parent class
+Constructor/Destructor chaining	How base and derived constructors/destructors 
+are called in order
+Access control (protected)	Using protected to allow subclass access to base attributes
+Polymorphism (early stage)	Not full virtual functions yet, but understanding how derived classes override behavior
+Method overriding	Specializing functions in the subclass
+Code reuse	Using inheritance to avoid copy-pasting base logic
+Well, when talking about inheritance usually the saying is "composition over inheritance"
+When doing systems design
+So instead of your X being a specialization of Y, your X "contains" a Y and uses it internally
+*/
