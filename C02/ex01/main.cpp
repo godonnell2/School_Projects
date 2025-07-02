1737bf96afd17c35ece804c4e72226af3b1a72ab
Grace <20680751+godonnell2@users.noreply.github.com>
2025-07-02 19:56:29 +0100

    new note

--- C02/ex01/main.cpp
+++ C02/ex01/main.cpp
@@ -4,9 +4,9 @@ int main(void) {
     Fixed a;
     Fixed const b(10);
     Fixed const c(42.42f);
-    Fixed const d(b);
+    Fixed const d(b); //copy constructor 
 
-    a = Fixed(1234.4321f);
+    a = Fixed(1234.4321f); //float constructor for a temp obj then assignemnt 
 
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
@@ -20,12 +20,12 @@ int main(void) {
 
     return 0;
 }
+
 /*
--- Creating b ---
-Float constructor called
--- Creating c (copy of b) ---
-Copy constructor called
-Copy assignment operator called
--- Assigning b to a ---
-Copy assignment operator called
+C++ chooses the constructor based on the type of the argument.
+You’re seeing one Destructor called line at the end because:
+The temporary Fixed(1234.4321f) gets destroyed
+42.42f * 256 = 10859.52
+roundf(10859.52) = 10860
+Then in toFloat(): 10860 / 256 = 42.421875 c is 42.4219 rounded to 4 decimal places by cot
 */
