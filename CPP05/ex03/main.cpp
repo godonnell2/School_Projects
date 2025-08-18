6f17902c873e7f003e2102122ffbb0e479b11689
Grace <20680751+godonnell2@users.noreply.github.com>
2025-08-18 20:07:27 +0100

    notes

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -20,3 +20,19 @@ int main(void)
 
 	delete trees;
 }
+
+/*
+Intern is a new class .
+Its job is to create forms dynamically.
+You don’t call new ShrubberyCreationForm directly in main; instead, you ask the intern to make it.
+The intern uses strings to decide which subclass to instantiate:
+
+"shrubbery creation" → ShrubberyCreationForm
+"robotomy request" → RobotomyRequestForm
+"presidential pardon" → PresidentialPardonForm
+Concept: Factory pattern — abstract object creation into a helper class.
+
+The intern returns a pointer to the base class AForm.
+At runtime, the actual derived class (ShrubberyCreationForm) is stored in the base pointer.
+This allows you to interact with forms uniformly (through the base class interface), while the actual behavior is determined by the derived type.
+*/
